a8285edff6c1966f2a7866af4faa840ae951d00f
alex <goldenbergInTouch@gmail.com>
2022-07-31 10:49:43 +0300

    new whatchdogs

--- .config/awesome/watchdogs/init.lua
+++ .config/awesome/watchdogs/init.lua
@@ -1,6 +1,5 @@
 local awful = require("awful")
 local commands = require("commons.commands")
-local naughty = require("naughty")
 
 local watchdogs = {}
 watchdogs.signals = {}
@@ -13,6 +12,9 @@ watchdogs.signals.cpu = watchdogs.signals.prfx .. 'cpu'
 watchdogs.signals.pow = watchdogs.signals.prfx .. 'pow'
 watchdogs.signals.temp = watchdogs.signals.prfx .. 'temp'
 watchdogs.signals.ps = watchdogs.signals.prfx .. 'ps'
+watchdogs.signals.weather = watchdogs.signals.prfx .. 'weather'
+watchdogs.signals.git_repos = watchdogs.signals.prfx .. 'git_repos'
+watchdogs.signals.sync_packages = watchdogs.signals.prfx .. 'sync'
 watchdogs.signals.diskroot = watchdogs.signals.prfx .. 'diskroot'
 watchdogs.signals.diskboot = watchdogs.signals.prfx .. 'diskboot'
 watchdogs.signals.diskhome = watchdogs.signals.prfx .. 'diskhome'
@@ -22,6 +24,9 @@ watchdogs.scripts[watchdogs.signals.cpu] = commands.cpu
 watchdogs.scripts[watchdogs.signals.pow] = commands.pow
 watchdogs.scripts[watchdogs.signals.temp] = commands.temp
 watchdogs.scripts[watchdogs.signals.ps] = commands.top_ps
+watchdogs.scripts[watchdogs.signals.weather] = commands.weather_info
+watchdogs.scripts[watchdogs.signals.git_repos] = commands.git_repos(cfg.repos_scan.scan_root_path)
+watchdogs.scripts[watchdogs.signals.sync_packages] = commands.sync_packages
 watchdogs.scripts[watchdogs.signals.diskroot] = commands.get_disk_root_info
 watchdogs.scripts[watchdogs.signals.diskboot] = commands.get_disk_boot_info
 watchdogs.scripts[watchdogs.signals.diskhome] = commands.get_disk_home_info
@@ -42,26 +47,71 @@ watchdogs.callbacks[watchdogs.signals.pow] = function(widget, stdout)
   awesome.emit_signal("sysstat::pow", tonumber(capacity), status)
 end
 
+local check_excluded_repo_path = function(w)
+
+  for k, path in pairs(cfg.repos_scan.exclude_paths) do
+    if w:find(path) then
+        return false
+    end
+  end
+
+  return true
+end
+
+watchdogs.callbacks[watchdogs.signals.git_repos] = function(widget, stdout)
+
+  local repos = {}
+  local index = 1
+  for w in stdout:gmatch("[^\r\n]+") do
+
+    local included = check_excluded_repo_path(w)
+
+    if included then
+      repo_info = {}
+      repo_info.vcs = 'git'
+      repo_info.icon = ''
+      repo_info.path = w:match('(.*)/.git')
+      repo_info.name = repo_info.path:match('.+/(.+)$')
+      repos[index] = repo_info
+      index = index + 1
+    end
+  end
+  awesome.emit_signal("sysstat::git_repos", repos)
+end
+
 watchdogs.callbacks[watchdogs.signals.temp] = function(widget, stdout)
   awesome.emit_signal("sysstat::temp", tonumber(stdout))
 end
 
+watchdogs.callbacks[watchdogs.signals.sync_packages] = function(widget, stdout, stderr, exitreason, exitcode)
+
+  local date_table = os.date("*t")
+  local hour, minute, second = date_table.hour, date_table.min, date_table.sec
+
+  local year, month, day = date_table.year, date_table.month, date_table.day   -- date_table.wday to date_table.day
+  local result = string.format("%d-%d-%d %d:%d:%d", year, month, day, hour, minute, second, ms)
+
+
+  awful.spawn.with_shell("echo '" .. result .. "' > " .. home_folder .. '/.config/awesome/.packages_sync_time')
+  awesome.emit_signal("sysstat::package_sync", result)
+
+end
+
 watchdogs.callbacks[watchdogs.signals.ps] = function(widget, stdout)
 
-  local lines_tbl = {}
-  local i = 1
-  local j = 1
-  for line in stdout:gmatch("[^\r\n]+") do
-    lines_tbl[i] = {}
-    for cell in stdout:gmatch("(.+)%s+") do
-      naughty.notify({text = tostring(cell) })
-      lines_tbl[i][j] = cell
-      j = j + 1
-    end
-    i = i + 1
-  end
+  ps_info = {}
+  local index  = 1
+  for w in stdout:gmatch("%S+") do
+    local info = {}
+    info.name = w:match("#(.+)-")
+    info.cpu = w:match("-(.+)__")
+    info.mem = w:match("__(.+)")
+    ps_info[index] = info
+    index = index + 1
 
-  awesome.emit_signal("sysstat::ps", lines_tbl)
+  end
+  awesome.emit_signal("sysstat::ps", ps_info)
+
 end
 
 
@@ -70,6 +120,13 @@ local disk_callback = function(sig, widget, stdout)
   local used = stdout:match('(.*) ')
   local available  = stdout:match(' (.*)')
   awesome.emit_signal(sig, tonumber(used), tonumber(available))
+end
+
+
+watchdogs.callbacks[watchdogs.signals.weather] = function(widget, stdout)
+  local status = stdout:match('code":([0-9]+)},"wind_mph')
+  local temperature = stdout:match('temp_c":(.+),"temp_f')
+  awesome.emit_signal("data:weather", status, temperature)
 end
 
 watchdogs.callbacks[watchdogs.signals.diskroot] = function(widget, stdout)
@@ -94,12 +151,18 @@ watchdogs.init = function()
     watchdogs.run(watchdogs.signals.cpu, 2)
     watchdogs.run(watchdogs.signals.pow, 2)
 
+    watchdogs.run(watchdogs.signals.ps, 2)
+
     watchdogs.run(watchdogs.signals.temp, 30)
 
-    watchdogs.run(watchdogs.signals.diskroot, 60)
-    watchdogs.run(watchdogs.signals.diskhome, 60)
-    watchdogs.run(watchdogs.signals.diskboot, 60)
-  --   watchdogs.run(watchdogs.signals.ps, 2)
+    watchdogs.run(watchdogs.signals.diskroot, 5)
+    watchdogs.run(watchdogs.signals.diskhome, 5)
+    watchdogs.run(watchdogs.signals.diskboot, 5)
+
+    watchdogs.run(watchdogs.signals.git_repos, 3600)
+    watchdogs.run(watchdogs.signals.weather, 120)
+    watchdogs.run(watchdogs.signals.sync_packages, 3600)
+
 end
 
 return watchdogs
