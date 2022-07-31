3dced4eacd062dedd64235b564336ea1af4f3349
alex <goldenbergInTouch@gmail.com>
2022-07-31 10:47:09 +0300

    Additional theme, removing images references

--- .config/awesome/theme/1/theme.lua
+++ .config/awesome/theme/1/theme.lua
@@ -16,7 +16,7 @@ theme.font        = theme.font_famaly..tostring(theme.font_size)
 
 theme.pallete_c1 = "#d17bed"
 theme.pallete_c2 = "#96eef2"
-theme.pallete_c3 = "#2f2b3b"
+theme.pallete_c3 = "#2f2b3a"
 theme.pallete_c4 = "#CCB8F0"
 theme.pallete_c5 = "#333170"
 theme.palette_c6 = "#4b455c"
@@ -97,52 +97,8 @@ theme.icons_font = "Font Awesome 6 Free "
 -- beautiful.variable in your rc.lua
 --theme.bg_widget = "#cc0000"
 
--- Define the image to load
-theme.titlebar_close_button_normal = themes_path.."titlebar/close_normal.png"
-theme.titlebar_close_button_focus  = themes_path.."titlebar/close_focus.png"
 
-theme.titlebar_minimize_button_normal = themes_path.."titlebar/minimize_normal.png"
-theme.titlebar_minimize_button_focus  = themes_path.."titlebar/minimize_focus.png"
-
-theme.titlebar_ontop_button_normal_inactive = themes_path.."titlebar/ontop_normal_inactive.png"
-theme.titlebar_ontop_button_focus_inactive  = themes_path.."titlebar/ontop_focus_inactive.png"
-theme.titlebar_ontop_button_normal_active = themes_path.."titlebar/ontop_normal_active.png"
-theme.titlebar_ontop_button_focus_active  = themes_path.."titlebar/ontop_focus_active.png"
-
-theme.titlebar_sticky_button_normal_inactive = themes_path.."titlebar/sticky_normal_inactive.png"
-theme.titlebar_sticky_button_focus_inactive  = themes_path.."titlebar/sticky_focus_inactive.png"
-theme.titlebar_sticky_button_normal_active = themes_path.."titlebar/sticky_normal_active.png"
-theme.titlebar_sticky_button_focus_active  = themes_path.."titlebar/sticky_focus_active.png"
-
-theme.titlebar_floating_button_normal_inactive = themes_path.."titlebar/floating_normal_inactive.png"
-theme.titlebar_floating_button_focus_inactive  = themes_path.."titlebar/floating_focus_inactive.png"
-theme.titlebar_floating_button_normal_active = themes_path.."titlebar/floating_normal_active.png"
-theme.titlebar_floating_button_focus_active  = themes_path.."titlebar/floating_focus_active.png"
-
-theme.titlebar_maximized_button_normal_inactive = themes_path.."titlebar/maximized_normal_inactive.png"
-theme.titlebar_maximized_button_focus_inactive  = themes_path.."titlebar/maximized_focus_inactive.png"
-theme.titlebar_maximized_button_normal_active = themes_path.."titlebar/maximized_normal_active.png"
-theme.titlebar_maximized_button_focus_active  = themes_path.."titlebar/maximized_focus_active.png"
-
-theme.wallpaper = themes_path.."background.png"
-
--- You can use your own layout icons like this:
-theme.layout_fairh = themes_path.."layouts/fairhw.png"
-theme.layout_fairv = themes_path.."layouts/fairvw.png"
-theme.layout_floating  = themes_path.."layouts/floatingw.png"
-theme.layout_magnifier = themes_path.."layouts/magnifierw.png"
-theme.layout_max = themes_path.."layouts/maxw.png"
-theme.layout_fullscreen = themes_path.."layouts/fullscreenw.png"
-theme.layout_tilebottom = themes_path.."layouts/tilebottomw.png"
-theme.layout_tileleft   = themes_path.."layouts/tileleftw.png"
-theme.layout_tile = themes_path.."layouts/tilew.png"
-theme.layout_tiletop = themes_path.."layouts/tiletopw.png"
-theme.layout_spiral  = themes_path.."layouts/spiralw.png"
-theme.layout_dwindle = themes_path.."layouts/dwindlew.png"
-theme.layout_cornernw = themes_path.."layouts/cornernww.png"
-theme.layout_cornerne = themes_path.."layouts/cornernew.png"
-theme.layout_cornersw = themes_path.."layouts/cornersww.png"
-theme.layout_cornerse = themes_path.."layouts/cornersew.png"
+theme.wallpaper = themes_path.."/1/background.png"
 
 
 theme.rounded = dpi(15)
