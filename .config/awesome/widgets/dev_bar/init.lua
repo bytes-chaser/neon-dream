432de45bbc2da0a202fc0c6e924b1aa0b49ac2ae
alex <goldenbergInTouch@gmail.com>
2022-08-07 12:59:07 +0300

    dev & stat bars refactoring

--- .config/awesome/widgets/dev_bar/init.lua
+++ .config/awesome/widgets/dev_bar/init.lua
@@ -1,39 +1,33 @@
-local awful                = require("awful")
-local wibox                = require("wibox")
-local beautiful            = require("beautiful")
-local dpi                  = beautiful.xresources.apply_dpi
-local pb                   = require("widgets.progress_bar")
-local shape_utils          = require("commons.shape")
-local mb                   = require("widgets.monitor_stat_bar_std")
-local monitor_panel        = require("widgets.monitor_panel")
-local disk_monitor_panel   = require("widgets.disk_monitor_panel")
-local processes            = require("widgets.processes")
-local repos                = require("widgets.repos")
-local card                 = require("widgets.card")
-local packages             = require("widgets.packages_list")
+local awful       = require("awful")
+local wibox       = require("wibox")
+local beautiful   = require("beautiful")
+local dpi         = beautiful.xresources.apply_dpi
+local shape_utils = require("commons.shape")
 
-local dev_bar = {}
+local card  = require("widgets.card")
+local packs = card.create_with_header("Packages", require("widgets.packages_list").create())
+local repos = card.create_with_header("Repositories", require("widgets.repos").create())
 
-dev_bar.create = function(s)
-  s.dev = awful.wibar({
-    position = "left",
-    screen   = s,
-    shape    = shape_utils.partially_rounded_rect(beautiful.rounded, true, true, true, true),
-    visible  = false,
-    width    = dpi(500),
-    height   = dpi(1000),
-    margins  = {
-      left = dpi(15)
+return {
+  create = function(s)
+    s.dev = awful.wibar({
+      position = "left",
+      screen   = s,
+      shape    = shape_utils.partially_rounded_rect(beautiful.rounded, true, true, true, true),
+      visible  = false,
+      width    = dpi(500),
+      height   = dpi(1000),
+      margins  = {
+        left = dpi(15)
+      }
+    })
+
+    s.dev:setup{
+      packs,
+      repos,
+      layout = wibox.layout.flex.vertical
     }
-  })
-
-  s.dev:setup{
-    card.create_with_header("Packages", packages.create()),
-    card.create_with_header("Repositories", repos.create()),
-    layout = wibox.layout.flex.vertical
-  }
 
 
-end
-
-return dev_bar
+  end
+}
--- .config/awesome/widgets/stat_bar/init.lua
+++ .config/awesome/widgets/stat_bar/init.lua
@@ -2,46 +2,51 @@ local awful                = require("awful")
 local wibox                = require("wibox")
 local beautiful            = require("beautiful")
 local dpi                  = beautiful.xresources.apply_dpi
-local pb                   = require("widgets.progress_bar")
 local shape_utils          = require("commons.shape")
-local mb                   = require("widgets.monitor_stat_bar_std")
 local monitor_panel        = require("widgets.monitor_panel")
 local disk_monitor_panel   = require("widgets.disk_monitor_panel")
 local processes            = require("widgets.processes")
 local card                 = require("widgets.card")
 
-stat_bar = {}
+local monitor_panel_params = monitor_panel.default_params()
+monitor_panel_params.direction   = 'east'
+monitor_panel_params.bars_margin = 10
 
-stat_bar.create = function(s)
-  s.stats = awful.wibar({
-    position = "left",
-    screen   = s,
-    shape    = shape_utils.partially_rounded_rect(beautiful.rounded, true, true, true, true),
-    visible  = false,
-    width    = dpi(300),
-    height   = dpi(1000),
-    margins  = {
-      left = dpi(15)
+local sys_headerless_mon_widget = monitor_panel.create(monitor_panel_params)
+local sys_mon_widget = card.create_with_header("System", sys_headerless_mon_widget)
+
+
+local processes_widget = card.create_with_header("Processes",  processes.create())
+
+
+local disk_monitor_panel_params = disk_monitor_panel.default_params()
+disk_monitor_panel_params.direction   = 'east'
+disk_monitor_panel_params.bars_margin = 10
+disk_monitor_panel_params.title_size  = 12
+
+local disks_headerless_mon_widget = disk_monitor_panel.create(disk_monitor_panel_params)
+local disks_mon_widget = card.create_with_header("Partitions", disks_headerless_mon_widget)
+
+return {
+  create = function(s)
+    s.stats = awful.wibar({
+      position = "left",
+      screen   = s,
+      shape    = shape_utils.partially_rounded_rect(beautiful.rounded, true, true, true, true),
+      visible  = false,
+      width    = dpi(300),
+      height   = dpi(1000),
+      margins  = {
+        left = dpi(15)
+      }
+    })
+
+    s.stats:setup{
+      sys_mon_widget,
+      processes_widget,
+      disks_mon_widget,
+      layout = wibox.layout.flex.vertical
     }
-  })
 
-  local monitor_panel_params = monitor_panel.default_params()
-  monitor_panel_params.direction   = 'east'
-  monitor_panel_params.bars_margin = 10
-
-  local disk_monitor_panel_params = disk_monitor_panel.default_params()
-  disk_monitor_panel_params.direction   = 'east'
-  disk_monitor_panel_params.bars_margin = 10
-  disk_monitor_panel_params.title_size  = 12
-
-  s.stats:setup{
-    card.create_with_header("System",     monitor_panel.create(monitor_panel_params)),
-    card.create_with_header("Processes",  processes.create()),
-    card.create_with_header("Partitions", disk_monitor_panel.create(disk_monitor_panel_params)),
-
-    layout = wibox.layout.flex.vertical
-  }
-
-end
-
-return stat_bar
+  end
+}
