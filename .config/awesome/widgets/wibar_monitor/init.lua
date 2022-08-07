6ce1a3f1c090408fa1d793fb2f18696b6a35bec7
alex <goldenbergInTouch@gmail.com>
2022-08-08 00:04:23 +0300

    wibar monitor refactoring

--- .config/awesome/widgets/wibar_monitor/init.lua
+++ .config/awesome/widgets/wibar_monitor/init.lua
@@ -2,61 +2,63 @@ local wibox       = require("wibox")
 local beautiful   = require("beautiful")
 local dpi         = beautiful.xresources.apply_dpi
 
-local wbm = {}
 
-wbm.create = function(label_text)
+return {
+  create = function(label_text)
+    local label = {
+      text   = label_text,
+      align  = 'center',
+      valign = 'center',
+      widget = wibox.widget.textbox
+    }
 
+    local percentage = {
+      id     = "wbm_valtext",
+      text   = '0%',
+      align  = 'center',
+      valign = 'center',
+      widget = wibox.widget.textbox
+    }
 
+    local graph = {
+      widget           = wibox.widget.graph,
+      min_value        = 0,
+      max_value        = 100,
+      step_spacing     = 5,
+      step_width       = 10,
+      scale            = true,
+      border_color     = beautiful.pallete_c1,
+      color            = beautiful.fg_normal,
+      background_color = beautiful.palette_c6,
+      id               = "wbm_graph"
+    }
 
-  return wibox.widget{
-    {
-      layout = wibox.layout.fixed.horizontal,
+    return wibox.widget{
       {
-        {
-          text = label_text,
-          align  = 'center',
-          valign = 'center',
-          widget = wibox.widget.textbox
-        },
-        {
-          id = "wbm_valtext",
-          text = '0%',
-          align  = 'center',
-          valign = 'center',
-          widget = wibox.widget.textbox
-        },
         layout = wibox.layout.fixed.horizontal,
-        id = "wbm_labels"
-      },
-      {
-        widget = wibox.container.margin,
-        left = dpi(10),
         {
-          {
-            widget = wibox.widget.graph,
-            min_value = 0,
-            max_value = 100,
-            step_spacing = 5,
-            step_width = 10,
-            scale = true,
-            border_color  = beautiful.pallete_c1,
-            color = beautiful.fg_normal,
-            background_color = beautiful.palette_c6,
-            id = "wbm_graph"
-          },
+          label,
+          percentage,
           layout = wibox.layout.fixed.horizontal,
-          id = "wbm_graphs"
+          id = "wbm_labels"
         },
-        id = "wbm_graphs_margin"
+        {
+          widget = wibox.container.margin,
+          left = dpi(10),
+          {
+            graph,
+            layout = wibox.layout.fixed.horizontal,
+            id     = "wbm_graphs"
+          },
+          id = "wbm_graphs_margin"
+        },
+        nil,
+        id = "wbm_body"
       },
-      nil,
-      id = "wbm_body"
-    },
-    widget = wibox.container.margin,
-    margins = dpi(10),
-  }
+      widget  = wibox.container.margin,
+      margins = dpi(10),
+    }
 
 
-end
-
-return wbm
+  end
+}
