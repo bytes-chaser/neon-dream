809790b309bae654df1816a19a929ed306ff6766
alex <goldenbergInTouch@gmail.com>
2022-08-07 18:30:24 +0300

    cards creating refactoring

--- .config/awesome/widgets/card/init.lua
+++ .config/awesome/widgets/card/init.lua
@@ -3,56 +3,47 @@ local beautiful       = require("beautiful")
 local dpi             = beautiful.xresources.apply_dpi
 local shape_utils     = require("commons.shape")
 
-local card = {}
+local function make_margin(content, margin)
 
-card.create = function(content)
-  return wibox.widget{
-    {
-      {
-        content,
-        widget = wibox.container.margin,
-        margins = dpi(15),
-      },
+  return {
+    content,
+    widget  = wibox.container.margin,
+    margins = margin or dpi(10),
+  }
+end
+
+return {
+  create = function(content, margin)
+    return wibox.widget(make_margin({
+      make_margin(content, margin),
       widget = wibox.container.background,
       bg = beautiful.palette_c6,
-    },
-    widget = wibox.container.margin,
-    margins = dpi(10),
-  }
-end
+    }, margin))
+  end,
 
+  create_with_header = function(header, content, margin)
+    local header_name = {
+          markup   = "<span foreground='" .. beautiful.fg_focus .."'>" .. header .."</span>",
+          font     = beautiful.font_famaly .. '14',
+          align    = "center",
+          opacity  = 1,
+          widget   = wibox.widget.textbox,
+    }
 
-card.create_with_header = function(header, content)
-  return wibox.widget{
-    {
+    return wibox.widget(make_margin({
       {
-        {
-              markup   = "<span foreground='" .. beautiful.fg_focus .."'>" .. header .."</span>",
-              font = beautiful.font_famaly .. '14',
-              align = "center",
-              opacity = 1,
-              widget = wibox.widget.textbox,
-        },
-        shape = shape_utils.partially_rounded_rect(beautiful.rounded, true, true, false, false),
+        header_name,
+        shape  = shape_utils.partially_rounded_rect(true, true, false, false),
         widget = wibox.container.background,
-        bg = beautiful.palette_c7,
+        bg     = beautiful.palette_c7,
       },
       {
-        {
-          content,
-          widget = wibox.container.margin,
-          margins = dpi(15),
-        },
-        shape = shape_utils.partially_rounded_rect(beautiful.rounded, false, false, true, true),
+        make_margin(content, margin),
+        shape  = shape_utils.partially_rounded_rect(false, false, true, true),
         widget = wibox.container.background,
-        bg = beautiful.palette_c6,
+        bg     = beautiful.palette_c6,
       },
       layout = wibox.layout.align.vertical
-    },
-    widget = wibox.container.margin,
-
-    margins = dpi(10),
-  }
-end
-
-return card
+    }, margin))
+  end
+}
