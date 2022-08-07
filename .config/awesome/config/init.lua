14a3e1dda6fdf7d25420dfac59ba6a239d612175
alex <goldenbergInTouch@gmail.com>
2022-08-07 18:23:36 +0300

    calendar settings. refactoring.

--- .config/awesome/config/init.lua
+++ .config/awesome/config/init.lua
@@ -3,21 +3,23 @@ local naughty 	  = require("naughty")
 
 cfg = {
 	theme_version = "2",
-	tags = { "web", "dev", "misc"},
-	tags_shape = shape_utils.default_frr,
+	tags = {
+		names = { "web", "dev", "misc"},
+		shape = shape_utils.default_frr
+	},
 	track_packages = {'git', 'lua', 'awesome-git', 'alacritty', 'zsh', 'python', 'go'},
 	repos_scan = {
 		scan_root_path = home_folder,
-		exclude_paths = {
+		exclude_paths  = {
 			home_folder .. '/.cache/yay/',
 			home_folder .. '/.local/share/nvim/'
 		}
 	},
 	calendar = {
 		week_started_on_sunday = true,
-		weekend_days_incices = {5, 6},
+		weekend_days_incices   = {5, 6},
 	},
-	todo ={
+	todo = {
 		path = home_folder .. '/.cache/awesome/todo/'
 	}
 }
