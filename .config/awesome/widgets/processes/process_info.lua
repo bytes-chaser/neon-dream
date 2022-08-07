b4f66ddb15a37b48a1c405a612e5c4fdfba3beab
alex <goldenbergInTouch@gmail.com>
2022-08-07 22:27:17 +0300

    simpifying process info factory

--- .config/awesome/widgets/processes/process_info.lua
+++ .config/awesome/widgets/processes/process_info.lua
@@ -1,30 +1,28 @@
 local wibox = require("wibox")
 
-local pi = {}
+return {
+  create = function(record)
+    local name = record.name
+    local cpu  = record.cpu
+    local mem  = record.mem
 
-pi.create = function(record)
-  local name = record.name
-  local cpu  = record.cpu
-  local mem  = record.mem
-
-  return wibox.widget{
-    {
-      id = "name",
-      widget = wibox.widget.textbox,
-      text = name
-    },
-    {
-      id = "cpu",
-      widget = wibox.widget.textbox,
-      text = cpu
-    },
-    {
-      widget = wibox.widget.textbox,
-      id = "mem",
-      text = mem
-    },
-    layout = wibox.layout.flex.horizontal,
-  }
-end
-
-return pi
+    return wibox.widget{
+      {
+        id = "name",
+        widget = wibox.widget.textbox,
+        text = name
+      },
+      {
+        id = "cpu",
+        widget = wibox.widget.textbox,
+        text = cpu
+      },
+      {
+        widget = wibox.widget.textbox,
+        id = "mem",
+        text = mem
+      },
+      layout = wibox.layout.flex.horizontal,
+    }
+  end
+}
