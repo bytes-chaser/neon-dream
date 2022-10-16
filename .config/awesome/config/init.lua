b9be9774f7fc6767532a9d68a3277e7cc2b4a85e
Alex <goldenbergInTouch@gmail.com>
2022-10-16 13:49:10 +0300

    disabling by default panels that have special dependencies, adding dependencies description to each panel with dependencies

--- .config/awesome/config/init.lua
+++ .config/awesome/config/init.lua
@@ -33,7 +33,11 @@ cfg = {
 			}
 		},
 		git = {
-			enabled = true,
+			--[[
+				Packages Required:
+				1. git
+			]]--
+			enabled = false,
 			scan_root_path = home_folder,
 			cache_file = home_folder .. '/.cache/awesome/repos',
 			pagination_defaults = {
@@ -50,10 +54,14 @@ cfg = {
 			}
 		},
 		docker = {
+			--[[
+				Packages Required:
+				1. docker
+			]]--
 			enabled = false,
 			cache_file = home_folder .. '/.cache/awesome/docker_containers',
 			pagination_defaults = {
-				size          = 10,     -- items per page,
+				size          = 10,    -- items per page,
 				order         = 'asc', -- asc or desc
 				sort_property = 3,     -- 1-ID, 2-Image, 3-Name, 4-Ports, 5-Status
 			},
@@ -62,11 +70,27 @@ cfg = {
 			}
 		},
 		stats = {
+
 			enabled = true,
 			version = 'v2' -- v1, v2
 		},
 		user = {
-			enabled = true,
+			--[[
+				Packages Required:
+				1. spotify
+				2. alsa-utils
+				3. pulseaudio
+				4. amixer
+				5. playerctl
+				5. brightnessctl
+
+				Action Required:
+				1. Setup Weather
+					1. Go to www.weatherapi.com and create your account
+					2. Export API Key into environment variable WEATHER_API_COM_API_KEY in .profile file
+					3. Export Your city location into environment variable WEATHER_API_COM_CITY in .profile file
+			]]--
+			enabled = false,
 		}
 	},
 
